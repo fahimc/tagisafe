8b8a0dddcb69bdc529de057e5c16c5a4fb7b2eb7
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2024-08-05 17:04:01 +0100

    added styles

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-  <h1>Contact Information</h1>
-<p id="personName"></p>
-<button class="button" id="callButton">Call Parent</button>
-<button class="button" id="whatsappButton">Send WhatsApp</button>
-<button class="button" id="mapsButton">Open in Google Maps</button>
-  </div>
-`;
-
 const params = new URLSearchParams(window.location.search);
 
 // Extract information from the query parameters
 const name = params.get("name");
 const phoneNumber = params.get("phone");
 const address = params.get("address");
+const avatar = params.get("avatar");
 
-// Update the page with the extracted information
-document.getElementById("personName").textContent = `Name: ${name}`;
+if (!address) {
+  document.getElementById("mapsButton").style.display = "none";
+} else {
+  document.getElementById("mapsButton").onclick = function () {
+    window.location.href = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+      address
+    )}`;
+  };
+}
 
-// Update button actions
+if (!name) {
+  document.getElementById(
+    "personName"
+  ).innerHTML = `My Name<br/><span class="name">UNKNOWN</span>`;
+} else {
+  document.getElementById(
+    "personName"
+  ).innerHTML = `My Name<br/><span class="name">${name}</span>`;
+}
+
+if (!avatar) {
+} else {
+  document.getElementById("avatar").src = `avatar/avatar-${avatar}.svg`;
+}
 document.getElementById("callButton").onclick = function () {
   window.location.href = `tel:${phoneNumber}`;
 };
 
 document.getElementById("whatsappButton").onclick = function () {
   window.location.href = `https://wa.me/${phoneNumber}`;
-};
-
-document.getElementById("mapsButton").onclick = function () {
-  window.location.href = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-    address
-  )}`;
 };
